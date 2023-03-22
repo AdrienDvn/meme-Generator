9de47d7b2c55d359c3b3be328f4f467d1acba06c
adriendavin <adriendavin@hotmail.fr>
2023-03-22 11:12:24 +0100

    building onClick events

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -5,13 +5,28 @@ export default function Meme() {
   // function handleMouseOver() {
   //   console.log("i was hovered")
   // }
-  function getMemeImage() {
-    const memesArray = memesData.data.memes
-    const randomNumber = Math.floor(Math.random()*memesArray.length)
-    const url = memesArray[randomNumber].url
-    console.log(url);
+   const thingsArray = ["Thing 1", "Thing 2"]
+   const things = thingsArray.map((item) => {
+    return (
+      <div key={item}>
+      <p>{item}</p>
+      </div>
+    )
+   })
 
-  }
+
+   function getAnotherThing() {
+      thingsArray.push(`Thing ${thingsArray.length + 1 }`);
+      console.log(thingsArray)
+    }
+
+  // function getMemeImage() {
+  //   const memesArray = memesData.data.memes
+  //   const randomNumber = Math.floor(Math.random()*memesArray.length)
+  //   const url = memesArray[randomNumber].url
+  //   console.log(url);
+
+  // }
 
   return (
     <main >
@@ -26,7 +41,9 @@ export default function Meme() {
            type="text"
            placeholder="Bottom text"
            />
-        <button onClick={getMemeImage} /*onMouseOver={handleMouseOver}*/ className='form--btn'> Get a new meme image 🖼</button>
+        <button onClick={getAnotherThing} /*onClick={getMemeImage}*/
+        /*onMouseOver={handleMouseOver}*/ className='form--btn'> Get a new meme image 🖼</button>
+        {things}
         </div>
     </main>
   )
