be2a2e55337124ef864552b6dccb33df87f571aa
adriendavin <adriendavin@hotmail.fr>
2023-03-23 11:49:14 +0100

    React.useState challenges

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,9 +1,18 @@
+import React from 'react';
 import memesData from '../memesData.js';
 
 export default function Meme() {
 
   // function handleMouseOver() {
   //   console.log("i was hovered")
+  // }
+
+  // function getMemeImage() {
+  //   const memesArray = memesData.data.memes
+  //   const randomNumber = Math.floor(Math.random()*memesArray.length)
+  //   const url = memesArray[randomNumber].url
+  //   console.log(url);
+
   // }
 
    const thingsArray = ["Thing 1", "Thing 2"]
@@ -20,36 +29,44 @@ export default function Meme() {
       console.log(thingsArray)
     }
 
-  // function getMemeImage() {
-  //   const memesArray = memesData.data.memes
-  //   const randomNumber = Math.floor(Math.random()*memesArray.length)
-  //   const url = memesArray[randomNumber].url
-  //   console.log(url);
-
-  // }
-
+    ///////////////////////////////////
+    ///////////////////////////////
   //state react challenge
-  function greeting(name1) {
-    const date = new Date()
-    const hours = date.getHours()
-    let timeOfDay;
+  // function greeting(name1) {
+  //   const date = new Date()
+  //   const hours = date.getHours()
+  //   let timeOfDay;
 
-    if (hours > 4 && hours < 12){
-      timeOfDay = "morning";
-    } else if(hours > 12 && hours < 17){
-      timeOfDay = "afternoon";
-    } else if(hours > 17 && hours < 20){
-      timeOfDay = "evening";
-    } else {
-      timeOfDay = "night";
+  //   if (hours > 4 && hours < 12){
+  //     timeOfDay = "morning";
+  //   } else if(hours > 12 && hours < 17){
+  //     timeOfDay = "afternoon";
+  //   } else if(hours > 17 && hours < 20){
+  //     timeOfDay = "evening";
+  //   } else {
+  //     timeOfDay = "night";
+  //   }
+  //   return `Good ${timeOfDay}, ${name1} !`;
+  // }
+  // greeting("Bob");
+
+
+const [isImportant, setIsImportant] = React.useState("wooowww")
+
+    function handleClick () {
+      setIsImportant("No")
     }
-    return `Good ${timeOfDay}, ${name1} !`;
-  }
-  greeting("Bob");
 
 
+//function to keep track of counts !
+    const [result2, setResult2] = React.useState(0)
 
-
+    function handleClick2 () {
+      setResult2(result2 - 1)
+    }
+    function handleClick3 () {
+      setResult2(result2 + 1)
+    }
 
   return (
     <main >
@@ -64,10 +81,20 @@ export default function Meme() {
            type="text"
            placeholder="Bottom text"
            />
-        <button onClick={getAnotherThing} /*onClick={getMemeImage}*/
+        <button /*onClick={handleClick}*/ /*onClick={getAnotherThing}*/ /*onClick={getMemeImage}*/
         /*onMouseOver={handleMouseOver}*/ className='form--btn'> Get a new meme image 🖼</button>
-        {things}
+
+        {/* {things} */}
+        <h1>{isImportant}</h1>
+
+        {/* Counter that keep counts challenge */}
+        <button onClick={handleClick2} className='counter--minus'>-</button>
+        <div className='counter--count'>
+          <h1>{result2}</h1>
         </div>
+        <button onClick={handleClick3}className='counter--minus'>+</button>
+
+      </div>
     </main>
   )
 }
