925e5fd95dbfb3845f097bd29aa98092122f54e2
adriendavin <adriendavin@hotmail.fr>
2023-04-11 16:33:21 +0200

    windowtracker challenge
    Signed-off-by: adriendavin <adriendavin@hotmail.fr>

--- src/App.js
+++ src/App.js
@@ -8,17 +8,30 @@ import Header from './components/Header.js';
 import Meme from './components/Meme.js';
 import memesData from './memesData.js';
 import ApiChallenge from './components/ApiChallenge';
+import WindowTracker from "./components/WindowTracker.js"
 
 function App() {
-  ////passing data through state challenge///
-  const [user, setUser] = useState('Bobby')
 
+  const [show, setShow] = useState(true)
+
+  function toggle() {
+    console.log('wassup');
+    return (
+      setShow(!show)
+    )
+  }
 
   return (
     <div className="App">
-      <Header user={user}/>
-      <Meme user={user}/>
-      <ApiChallenge/>
+      {/* <Header />
+      <Meme /> */}
+      {/* <ApiChallenge/> */}
+
+
+      <button onClick={toggle}>
+        Toggle WindowTracker
+      </button>
+      {show && <WindowTracker />}
     </div>
   );
 }
---
+++ src/components/WindowTracker.js
@@ -0,0 +1,7 @@
+import React, {useState, useEffect} from 'react'
+
+export default function WindowTracker(props) {
+  return (
+    <h1>Window Width: {window.innerWidth}</h1>
+  )
+}
